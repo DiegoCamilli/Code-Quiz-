6ef14eca2a759a840f2e4e38ffdf01f67a6fdde1
DiegoCamilli <camillidiego@yahoo.com>
2023-05-11 19:41:15 -0700

    started JS file content and wrote HTML

---
+++ script.js
@@ -0,0 +1,53 @@
+// array of obj that hold the questions
+const questionBank = [
+    {
+        title: 'Commonly used data types DO NOT incluse:',
+        options: ['strings', 'alerts', 'boolean', 'numbers'],
+        answer: 'alerts',
+    },
+    {
+        title: 'The conditions in an if/else statement is enclosed with _______.',
+        options: ['quotes', 'curly brackets', 'oarenthesis', 'square brackets'],
+        answer: 'parenthesis',
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store _______.',
+        options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        answer: 'all of the above',
+    },
+    {
+        title: 'String values msut be enclosed with _______ when being assinged to a variable.',
+        options: ['parenthesis', 'quotes', 'commas', 'square brackets'],
+        answer: 'quotes',
+    },
+    {
+        title: 'A very useful tool for debugging during develpment and debugging for printing content to the debugger.',
+        options: ['console.log', 'for loop', 'JavaScript', 'terminal/bash'],
+        answer: 'console.log',
+    },
+    {
+        title: 'Which one of these IS NOT a variable type.',
+        options: ['var', 'let', 'const', 'Math'],
+        answer: 'Math',
+    },
+    {
+        title: 'A number that is a decimal is also known as a _______ number.',
+        options: ['max', 'floor', 'float', 'min'],
+        answer: 'float',
+    },
+    {
+        title: 'True or False: Numbers writin in string form are affected by Math methods or expressions.',
+        options: ['True', 'False'],
+        answer: 'False',
+    },
+    {
+        title: 'In order to use index elements in an array, we use _______.',
+        options: ['dot notation', 'square bracket notation', 'query selector', 'get element by id'],
+        answer: 'square bracket notation',
+    },
+    {
+        title: 'An array starts at 0 because they are _______.',
+        options: ['non incremental', 'zero indexed', 'empty', 'undefined'],
+        answer: 'zero indexed',
+    },
+]
